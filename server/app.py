2f8015a8832045d4d58f68f8d5fd5e9c75d539c8
Augustine Meso <augustinemesobk@gmail.com>
2025-01-22 16:31:22 +0300

    add updated app.py

--- server/app.py
+++ server/app.py
@@ -1,5 +1,18 @@
-#!/usr/bin/env python3
-
 from flask import Flask
 
+# Create an instance of the Flask class
 app = Flask(__name__)
+
+# Define a route for the home page
+@app.route('/')
+def index():
+    return '<h1>Welcome to my page!</h1>'
+
+# Define a route that accepts a dynamic username
+@app.route('/<string:username>')
+def user(username):
+    return f'<h1>Profile for {username}</h1>'
+
+# Run the application on port 5555 in debug mode
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
